3c6270a9cb4684e3cf501c8b09792713f5ba9963
Alceu-Haruo <alceu.haruo@uol.com.br>
2022-03-31 10:58:09 -0300

    2022.03.31

---
+++ Health_expenditures.R
@@ -0,0 +1,5 @@
+## health expeditures
+
+
+health_coverage <- read.table("healthcare-coverage.csv", sep = ",", skip = 2, header = T, nrows = 52)
+health_spend <- read.table("healthcare-spending.csv", sep = ",", skip = 2, header = T, nrows = 52)
